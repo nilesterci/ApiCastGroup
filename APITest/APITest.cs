a08b6f46660e134014068899a4b38e897292fca9
nilesterci <nilmar67@gmail.com>
2023-08-06 23:05:18 -0300

    Xunit

---
+++ APITest/APITest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using Xunit;
+
+namespace APITest
+{
+    public class APITest
+    {
+        [Fact]
+        public async void GetAllCustomersTest()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            var response = await client.GetAsync("/consultaCep/13174010");
+            var data = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
